1d60d6570e8d2723c60b726068f8c7c25e31a966
exit4525-ops <exit4525@gmail.com>
2026-09-08 14:53:06 +0300

    commit: Добавлена структура трубы и ввод данных

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+struct Pipe
+{
+    string name;
+    double length;
+    int diameter;
+    bool repair;
+};
 
 int main()
 {
+    Pipe pipe;
+
     cout << "Program started" << endl;
 
     return 0;
